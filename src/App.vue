22dee01fea0ffa92c40444078ea65d2bc8a8ab2d
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-08-27 22:02:46 +0300

    vue project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+  #app {
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/components/DashboardComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dashboard">
+    <div class="sidebar">
+      <div class="personal-wrapper">
+        <div class="img-wrapper" :style="{ backgroundImage: `url(${require('@/assets/image-jeremy.png')})` }"></div>
+        <div class="text-wrapper">
+          <p>Report for</p>
+          <h1>Jeremy Robson</h1>
+        </div>
+      </div>
+      <div class="route-wrapper">
+        <router-link to="/">Daily</router-link>
+        <router-link to="/weekly-view">Weekly</router-link>
+        <router-link to="/monthly-view">Monthly</router-link>
+      </div>
+    </div>
+    <!-- here is list rendering -->
+    <div class="card-grid-wrapper">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardComponent",
+  props: {
+    profile: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "src/styles/dashboard.scss";
+</style>
